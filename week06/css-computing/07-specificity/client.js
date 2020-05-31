ba86be6f18ce56eb69ae4f1f66fe0c07bd6ddf43
owensiz <18616778118@163.com>
2020-05-31 23:01:10 +0800

    update week6

--- week06/css-computing/07-specificity/client.js
+++ week06/css-computing/07-specificity/client.js
@@ -237,5 +237,5 @@ void (async function () {
   });
   let response = await request.send();
   let dom = parser.parseHTML(response.body);
-  console.log('dom',dom);
+  console.log('------dom------', dom);
 })();
